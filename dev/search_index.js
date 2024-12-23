dc379bbe16c50a630948bd528563a428a8c405fb
Documenter.jl <documenter@juliadocs.github.io>
2024-12-23 02:31:57 +0000

    build based on ef530ae

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PackageofABC","category":"page"},{"location":"#PackageofABC","page":"Home","title":"PackageofABC","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PackageofABC.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PackageofABC]","category":"page"}]
+}
